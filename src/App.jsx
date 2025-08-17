7607232ef49de06d0efc854f85c3fb551ca1518d
Amna Janahi <amna.m.janahi@gmail.com>
2025-08-17 14:36:49 +0300

    adding app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState, useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+import { PulseLoader } from "react-spinners";
 
-  return (
+import BookingForm from "./components/BookingForm/BookingForm";
+
+
+const App = () => {
+  const [formIsShown, setFormIsShown] = useState(false)
+
+  const handleShowFormClick = () => {
+    console.log('show form button is clicked')
+    setFormIsShown(true)
+  }
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Welcome</h1>
+    <button onClick={handleShowFormClick}>Book your ticket</button>
+      {
+        formIsShown
+          ?
+          <BookingForm  setFormIsShown={setFormIsShown}/>
+          :
+          <BookingList />
+        
+      }
     </>
   )
+
 }
 
 export default App
