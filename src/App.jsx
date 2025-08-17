1a8ae04750a2f9603dd25fc2b01166da18303974
Amna Janahi <amna.m.janahi@gmail.com>
2025-08-17 14:38:53 +0300

    returning BookingForm in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
           ?
           <BookingForm  setFormIsShown={setFormIsShown}/>
           :
-          <BookingList />
+          <p>The list</p>
         
       }
     </>
