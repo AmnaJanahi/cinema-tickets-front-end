89178c8ca6e3b109f424e26d86545643f07ab2c0
Amna Janahi <amna.m.janahi@gmail.com>
2025-08-17 14:27:29 +0300

    adding BookingForm return statment

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -21,7 +21,33 @@ const BookingForm = () => {
 
   return (
     <>
+      <h2>Book Your Ticket</h2>
+      <form onSubmit={handelSubmite}>
+        <lable htmlFor="name">Name</lable>
+        <input
+          id="name"
+          name="name"
+          onChange={handelChange}
+          value={formData.name}
+        />
+        <lable htmlFor="date">Date</lable>
+        <input
+          id="date"
+          name="date"
+          onChange={handelChange}
+          value={formData.date}
+        />
+        <lable htmlFor="timing">Timing</lable>
+        <input
+          id="timing"
+          name="timing"
+          onChange={handelChange}
+          value={formData.timing}
+        />
+        {/* adding seats */}
+        <button type="submit">Submit</button>
+      </form>
     </>
-  )
+  );
 };
 export default BookingForm;
