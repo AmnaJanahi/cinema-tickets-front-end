39171e80ca4a5a47ec3a260218ad17a24e01e041
Amna Janahi <amna.m.janahi@gmail.com>
2025-08-18 11:46:30 +0300

    importing auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import {jwtDecode} from 'jwt-decode'
+
+import LoginForm from './LoginForm'
+import SignUp from './SignupForm'
+import LogoutButton from './LogoutButton'
+import ProtectedRoute from './ProtectedRoute'
+
 import axios from "axios"
 import { useState, useEffect } from "react"
 
@@ -7,6 +14,10 @@ import BookingForm from "./components/BookingForm/BookingForm";
 import BookingList from "./components/BookingList/BookingList";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  
+
   const [formIsShown, setFormIsShown] = useState(false)
 
   const handleShowFormClick = () => {
